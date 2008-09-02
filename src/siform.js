6ddc9f21d9adcb6bfbb5ab6da01775104b2e8b96
Dominik <znidar@gmail.com>
2008-09-02 22:39:45 +0200

    started working on styles

--- src/siform.js
+++ src/siform.js
@@ -17,7 +17,13 @@ var SiForm = Class.create({
 			method: "post"
 		}, this.options.formOptions);
 		if (!this.options.formOptions.id) this.options.formOptions.id = "siform_"+Math.round(Math.random()*100000);
+		this.formId = this.options.formOptions.id;
 		this.element.update(this.buildForm());
+		if (this.options.displayType == "list") {
+			mw = SiForm.Tools.fixWidth(".sf-label", 5);
+			SiForm.Tools.setStyle(".sf-input-div, .sf-buttons", { paddingLeft: mw+"px" });
+			SiForm.Tools.setStyle("#"+this.formId+" input[type='text'], #"+this.formId+" select, #"+this.formId+" textarea", { width: (this.options.width - mw - 10)+"px" });
+		}
 		Event.observe(this.options.formOptions.id, 'submit', this.validateForm.bindAsEventListener(this));
 	},
 
@@ -26,17 +32,17 @@ var SiForm = Class.create({
 		var formElements = this.getFormElements(this.options.elements);
 		var formButtons = this.getFormButtons();
 		if (this.options.displayType == 'table') {
-			wrapper = ["table", { cellpadding: 0, cellspacing: 0 }, [
+			wrapper = ["table", { cellpadding: 0, cellspacing: 0, className: "sf-table" }, [
 				["tbody", {}, [].concat(formElements, formButtons) ]
 			]];
 		} else {
-			wrapper = ["ul", {}, [].concat(formElements, formButtons)];
+			wrapper = ["div", { className: "sf-form-content" }, [].concat(formElements, formButtons)];
 		}
 		return this.createElement("form", { 
 			action: this.options.formOptions.action,
 			method: this.options.formOptions.method,
-			id: this.options.formOptions.id//,
-			//onSubmit: "console.log($F('"+this.options.formOptions.id+"'))"
+			id: this.options.formOptions.id,
+			className: "sf-form"
 		}, [wrapper]);
 	},
 
@@ -60,18 +66,18 @@ var SiForm = Class.create({
 	},
 
 	getFormButtons: function() {
-		return [["ul", { className: 'buttons' }, [["li", {}, [["input", { type: "submit" }]]]]]];
+		return [["div", { className: 'sf-buttons' }, [["input", { type: "submit" }]]]];
 	},
 
 	labelElement: function(element, elementOptions) {
-		label = ["label", { htmlFor: "f_"+elementOptions.name }, [elementOptions.title]];
+		label = ["label", { htmlFor: "f_"+elementOptions.name, className: "sf-label" }, [elementOptions.title]];
 		if (this.options.displayType == "table") {
 			return ["tr", {}, [
-				["td", {}, [label]],
-				["td", {}, [element]]
+				["td", { className: "sf-label-cell" }, [label]],
+				["td", { className: "sf-element-cell" }, [element]]
 			]];
 		} else {
-			return ["li", {}, [label, element]];
+			return ["div", { className: "sf-form-element" }, [label, ["div", { className: "sf-input-div" }, [element]]]];
 		}
 	},
 
@@ -115,7 +121,6 @@ var SiForm = Class.create({
 				}
 			}
 		}
-
 		if (foundErrors) Event.stop(e);
 	},
 
@@ -140,7 +145,7 @@ SiForm.Elements = {
 		}, options || {});
 
 		// build element
-		return ['input', { name: options.name, value: options.value, id: 'f_'+options.name }];
+		return ['input', { type: "text", name: options.name, value: options.value, id: 'f_'+options.name, className:"sf-textfield" }];
 	},
 	
 	textarea: function(options) {
@@ -152,7 +157,7 @@ SiForm.Elements = {
 		}, options || {});
 		
 		// build element
-		return ["textarea", { name: options.name, id: "f_"+options.name, rows: options.rows }, [options.value]];
+		return ["textarea", { name: options.name, id: "f_"+options.name, rows: options.rows, className:"sf-textarea" }, [options.value]];
 	},
 	
 	select: function(options) {
@@ -183,7 +188,7 @@ SiForm.Elements = {
 			}
 			els.push(["option", Object.extend(value==options.value ? { selected: "1" } : {}, { value: value }), [title]]);
 		}
-		return ["select", Object.extend(options.multiple ? { multiple: "1", size: options.size } : {}, { name: options.name, id: "f_"+options.name }), els];
+		return ["select", Object.extend(options.multiple ? { multiple: "1", size: options.size } : {}, { name: options.name, id: "f_"+options.name, className: "sf-select" }), els];
 	},
 	
 	radio : function(options) {
@@ -205,10 +210,11 @@ SiForm.Elements = {
 			} else {
 				value = title = options.values[i];
 			}
-			els.push(["li", {}, [["input", Object.extend(value==options.value ? { checked: "1" } : {}, { type: "radio", name: options.name, value: value, id: "f_"+options.name+"_"+value.underscore()})]]]);
-			els.push(["li", {}, [["label", { htmlFor: "f_"+options.name+"_"+value.underscore()}, [title]]]]);
+			label = ["input", Object.extend(value==options.value ? { checked: "1" } : {}, { type: "radio", name: options.name, value: value, id: "f_"+options.name+"_"+value.underscore(), className: "sf-radio"})];
+			radioel = ["label", { htmlFor: "f_"+options.name+"_"+value.underscore(), className: "sf-sub-label"}, [title]];
+			els.push(["div", { className: "sf-sub-element" }, [label, radioel]]);
 		}
-		return ["ul", {}, els];
+		return ["div", { className: " sf-radios" }, els];
 	},
 	
 	checkbox : function(options) {
@@ -231,10 +237,11 @@ SiForm.Elements = {
 			} else {
 				value = title = options.values[i];
 			}
-			els.push(["li", {}, [["input", Object.extend(options.value.indexOf(value)>-1 ? { checked: "1" } : {}, { type: "checkbox", name: options.name, value: value, id: "f_"+options.name+"_"+value.underscore()})]]]);
-			els.push(["li", {}, [["label", { htmlFor: "f_"+options.name+"_"+value.underscore()}, [title]]]]);
+			label = ["input", Object.extend(options.value.indexOf(value)>-1 ? { checked: "1" } : {}, { type: "checkbox", name: options.name, value: value, id: "f_"+options.name+"_"+value.underscore(), className: "sf-checkbox"})];
+			checkel = ["label", { htmlFor: "f_"+options.name+"_"+value.underscore(), className:"sf-sub-label"}, [title]];
+			els.push(["div", { className: "sf-sub-element" }, [label, checkel]]);
 		}
-		return ["ul", {}, els];
+		return ["div", { className: " sf-checkboxes" }, els];
 	}
 };
 
@@ -244,7 +251,21 @@ SiForm.Validations = {
 		message: "This field is required!"
 	},
 	email: {
-		pattern: /(.+)/,
+		pattern: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
 		message: "This in not a valid e-mail address!"
 	}
 }
+
+SiForm.Tools = {
+	fixWidth: function(selector, margin) {
+		var maxWidth = 0;
+		$$(selector).each(function(el) { width = el.getWidth(); maxWidth = width > maxWidth ? width : maxWidth; });
+		if (margin) maxWidth += margin;
+		$$(selector).each(function(el) { el.style.width = maxWidth+"px"; });
+		return maxWidth;
+	},
+	setStyle: function(selector,styles) {
+		var setStyleStyles = styles;
+		$$(selector).each(function(el) { el.setStyle(setStyleStyles); });
+	}
+};
